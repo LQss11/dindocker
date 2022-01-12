7789e1b41a8ebf8ea2c04a6e7c4f252493e92c8a
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-01-12 20:38:29 +0100

    update join and purge scripts for alpine dind

--- src/swarm/join.sh
+++ src/swarm/join.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-EI=$(/sbin/ifconfig eth0 | grep inet | awk '{ print $2}') # Get inet IP of eth0
-SCAN=$(nmap $EI/24 | grep dindocker)
+docker swarm init                                             # Create a docker swarm
+WORKER_TOKEN=$(docker swarm join-token worker | grep token)   # get full worker token
+MANAGER_TOKEN=$(docker swarm join-token manager | grep token) # get full manager token
+
+if cat /proc/version | grep -q Alpine; then
+    # We are using an alpine container
+    EI=$(/sbin/ifconfig eth0 | grep inet | cut -f2 -d":" | awk '{ print $1}') # Get inet IP of eth0
+
+else
+    # We are using an Ubuntu container
+    EI=$(/sbin/ifconfig eth0 | grep inet | awk '{ print $2}') # Get inet IP of eth0
+
+fi
+
+SCAN=$(nmap $EI/24 | grep dindocker)                             # Scan Network
 IPS=($(echo "$SCAN" | awk '{ print $6}' | sed 's/.$//; s/^.//')) # Look for the nodes Ip addresses
 NAMES=($(echo "$SCAN" | awk '{ print $5}' | cut -f1 -d"."))      # Look for the nodes Containers names
 
 echo "host IP= $EI"
-
-docker swarm init                                             # Create a docker swarm
-WORKER_TOKEN=$(docker swarm join-token worker | grep token)   # get full worker token
-MANAGER_TOKEN=$(docker swarm join-token manager | grep token) # get full manager token
 
 for i in "${!IPS[@]}"; do #loop through IP addresses found
 
--- src/swarm/purge.sh
+++ src/swarm/purge.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-EI=$(/sbin/ifconfig eth0 | grep inet | awk '{ print $2}') # Get inet IP of eth0
-SCAN=$(nmap $EI/24 | grep dindocker)
+if cat /proc/version | grep -q Alpine; then
+    # We are using an alpine container
+    EI=$(/sbin/ifconfig eth0 | grep inet | cut -f2 -d":" | awk '{ print $1}') # Get inet IP of eth0
+
+else
+    # We are using an Ubuntu container
+    EI=$(/sbin/ifconfig eth0 | grep inet | awk '{ print $2}') # Get inet IP of eth0
+
+fi
+
+SCAN=$(nmap $EI/24 | grep dindocker)                             # Scan Network
 IPS=($(echo "$SCAN" | awk '{ print $6}' | sed 's/.$//; s/^.//')) # Look for the nodes Ip addresses
 NAMES=($(echo "$SCAN" | awk '{ print $5}' | cut -f1 -d"."))      # Look for the nodes Containers names
 
 echo "host IP= $EI"
 docker swarm leave -f
-
 for i in "${!IPS[@]}"; do #loop through IP addresses found
 
     echo "Hostname= ${NAMES[$i]} | IP= ${IPS[$i]}"
     sshpass -p root ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${IPS[$i]} docker swarm leave -f
-
 done
